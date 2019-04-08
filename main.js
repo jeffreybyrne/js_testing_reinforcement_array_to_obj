4ddb3908628daa12d0ceacd556441353994f3e0b
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-04-08 13:47:37 -0400

    finished the exercise

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function arrayToObj() {
-   // your code goes here  
+function arrayToObj(inputArray) {
+   
+   const outputArray = {};
+   inputArray.map(pairObj => {
+      director = pairObj[0];
+      movie = pairObj[1];
+      outputArray[director] = movie;
+   })
+   return outputArray;
 }
 
 module.exports = arrayToObj;
